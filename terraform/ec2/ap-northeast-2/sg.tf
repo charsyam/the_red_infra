f14d08f3cf7e6c3c08a856e792bce82061695231
Ubuntu <ubuntu@ip-172-31-14-202.ap-northeast-2.compute.internal>
2021-07-08 16:05:48 +0000

    remove ipv6

--- terraform/ec2/ap-northeast-2/sg.tf
+++ terraform/ec2/ap-northeast-2/sg.tf
@@ -40,7 +40,6 @@ resource "aws_security_group" "allow_the_red" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
