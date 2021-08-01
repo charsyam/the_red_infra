d0ebaf9a441ba0c5619c145f491bbc4ae0bec850
charsyam <charsyam@naver.com>
2021-08-01 14:16:52 +0900

    using ostype to download terraform

--- install_terraform.sh
+++ install_terraform.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+case "$OSTYPE" in
+    darwin*) FILENAME="darwin_amd64" ;;
+    *)       FILENAME="linux_amd64" ;;
+esac
+
 mkdir tmp
 cd tmp
-wget https://releases.hashicorp.com/terraform/1.0.1/terraform_1.0.1_linux_amd64.zip tmp/
+wget https://releases.hashicorp.com/terraform/1.0.1/terraform_1.0.1_${FILENAME}.zip tmp/
 unzip ./terraform_1.0.1_linux_amd64.zip
 sudo cp terraform /usr/bin
 cd ..
