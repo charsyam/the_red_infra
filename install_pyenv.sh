5db20a4be3ef150eaa376119a389cb8d428c6e53
charsyam <charsyam@naver.com>
2021-08-01 12:25:18 +0900

    fix install_pyenv.sh

--- install_pyenv.sh
+++ install_pyenv.sh
@@ -8,14 +8,15 @@ PYTHON_VERSION="3.8.6"
 VIRTUAL_ENV="the-red-dev"
 PYENV_DIR=~/.pyenv
 
-MYSHELL=`echo "$SHELL" | awk -F: '{print $NF}'`
+MYSHELL=`echo "$SHELL" | awk -F/ '{print $NF}'`
+echo "SHELL: $MYSHELL"
 if [ ! -d $PYENV_DIR ]; then
     git clone https://github.com/yyuu/pyenv.git $PYENV_DIR
     git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 
-    TARGET="~/.bashrc"
-    if ["$MYSHELL" = "zsh"]; then
-        TARGET="~/.zshrc"
+    TARGET=~/.bashrc
+    if [ "$MYSHELL" = "zsh" ]; then
+        TARGET=~/.zshrc
     fi
 
     echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $TARGET
