7485ef8655416dc119e852e24fb7542a8d22e3bd
charsyam <charsyam@naver.com>
2021-08-01 11:55:59 +0900

    update with zsh

--- install_pyenv.sh
+++ install_pyenv.sh
@@ -8,13 +8,21 @@ PYTHON_VERSION="3.8.6"
 VIRTUAL_ENV="the-red-dev"
 PYENV_DIR=~/.pyenv
 
+MYSHELL=`echo "$SHELL" | awk -F: '{print $NF}'`
 if [ ! -d $PYENV_DIR ]; then
     git clone https://github.com/yyuu/pyenv.git $PYENV_DIR
     git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-    echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
-    echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+    TARGET="~/.bashrc"
+    if ["$MYSHELL" = "zsh"]; then
+        TARGET="~/.zshrc"
+    fi
+
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $TARGET
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> $TARGET
+    echo 'eval "$(pyenv init --path)"' >> $TARGET
+    echo 'eval "$(pyenv virtualenv-init -)"' >> $TARGET
+
+    echo "source $TARGET"
 fi
 
-echo "source ~/.bashrc"
