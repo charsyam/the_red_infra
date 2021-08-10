5d2cc636093b0e7e761cf35f7b594cdf84d1faaf
charsyam <charsyam@naver.com>
2021-08-11 01:36:34 +0900

    remain only 1 type

--- scripts/bluegreen_deploy.py
+++ scripts/bluegreen_deploy.py
@@ -16,8 +16,9 @@ def deploy(deploy_conf):
     lb_name = deploy_config["lb_name"]
     subnets = deploy_config["subnets"]
     sgs = deploy_config["sgs"]
-    tg_conf = deploy_config["tg_conf"]
+    tgs_conf = deploy_config["tgs_conf"]
     asg_conf = deploy_config["asg_conf"]
+    listener_conf = deploy_config["listener_conf"]
 
     lb = None
     try:
@@ -32,16 +33,31 @@ def deploy(deploy_conf):
     lb_arn = lb["LoadBalancerArn"]
     vpc_id = lb["VpcId"]
 
-    tgs = get_tgs(elb_client, lb_arn)
-    tg = get_tg_by_name(tgs, deploy_config["tg_conf"]["name"])
-    if not tg:
-        new_tg = create_tg(elb_client, tg_conf, vpc_id)[0]
-        tg_arn = new_tg["TargetGroupArn"]
-        listener_conf = deploy_config["listener_conf"]
-        create_listener(elb_client, listener_conf, tg_arn, lb_arn)
-        tg = new_tg
+    tgs_map = {}
+    for tg_conf in tgs_conf:
+        try:
+            tg = get_tgs_by_name(elb_client, [tg_conf["name"]])[0]
+            print(tg)
 
-    
+            tgs_map[tg_conf["name"]] = tg
+        except Exception as e:
+            new_tg = create_tg(elb_client, tg_conf, vpc_id)[0]
+            tgs_map[tg_conf["name"]] = new_tg
+        
+        
+    tg = tgs_map[deploy_conf["target_tg"]]
+    tgs_weights = []
+
+    for tg_key in tgs_map.keys():
+        tmp_tg = tgs_map[tg_key]
+        weight = 0
+        if tmp_tg == tg:
+            weight = 1
+
+        tgs_weights.append({"TargetGroupArn": tmp_tg["TargetGroupArn"], "Weight": weight})
+        
+    tg_arn = tg["TargetGroupArn"]
+
     tpl_conf = deploy_config["tpl_conf"]
     tpls = get_launch_tpls(ec2_client, tpl_conf["name"])
 
@@ -64,27 +80,30 @@ def deploy(deploy_conf):
         asg_old_num_id = get_max_id(asgs, "AutoScalingGroupName")
         asg_num_id = asg_old_num_id + 1
 
-    old_total_count, old_healthy_count, old_targets = get_target_instances(check_target_health(elb_client, tg["TargetGroupArn"]))
-
     asg_name = asg_conf["name"] + f"_{asg_num_id}"
 
     tpl = create_launch_tpl(ec2_client, tpl_name, tpl_conf, sgs)
     tpl_id = tpl["LaunchTemplateId"]
 
-    asgs = create_asg(asg_client, tpl_id, tg["TargetGroupArn"], asg_name, asg_conf, subnets)
+    listeners = get_listeners(elb_client, lb_arn)
+    listener = listeners[0]
+    listener_arn = listener["ListenerArn"]
+
+    asgs = create_asg(asg_client, tpl_id, tg_arn, asg_name, asg_conf, subnets)
     while True:
-        targets = check_target_health(elb_client, tg["TargetGroupArn"])
+        targets = check_target_health(elb_client, tg_arn)
+        print(targets)
         tc, hc, new_targets = get_target_instances(targets)
-        if hc >= old_healthy_count + asg_conf["desired_capacity"]:
-            print("deploy is success")
+        if hc >= asg_conf["desired_capacity"]:
+            print("target group is healty")
             break
         else:
-            print(f"total: {tc}, old_healthy: {old_healthy_count}, current_healthy_count: {hc}") 
+            print(f"total: {tc}, current_healthy_count: {hc}") 
 
         time.sleep(5)
 
-    targets = check_target_health(elb_client, tg["TargetGroupArn"])
-    new_tc, new_hc, new_targets = get_target_instances(targets)
+    print("change listerner for target group")
+    modify_listener(elb_client, listener_conf, listener_arn, tgs_weights)
 
     old_asg_name = asg_conf["name"] + f"_{asg_old_num_id}"
     set_desired_capacity_as_zero(asg_client, old_asg_name)
